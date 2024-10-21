5ca5e873ee71354fa705c60f9c0aa6240d3d3ec4
Dylan Murphy <dmurphy6@uoregon.edu>
2024-10-21 12:25:35 -0700

    implementing cp

--- command.c
+++ command.c
@@ -52,12 +52,50 @@ void changeDir(char *dirName) {
 
 /*for the cp command*/
 void copyFile(char *sourcePath, char *destinationPath) {
-    
+    // Open the source file for reading
+    int src_fd = open(sourcePath, O_RDONLY);
+    if (src_fd == -1) {
+        perror("Error opening source file");
+        return;
+    }
+
+    // Open (or create) the destination file for writing, with appropriate permissions
+    int dest_fd = open(destinationPath, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (dest_fd == -1) {
+        perror("Error opening destination file");
+        close(src_fd);  // Make sure to close the source file if destination fails
+        return;
+    }
+
+    // Buffer to hold the file content as it is copied
+    char buffer[1024];
+    ssize_t bytesRead;
+
+    // Read from the source and write to the destination until reaching the end of the source file
+    while ((bytesRead = read(src_fd, buffer, sizeof(buffer))) > 0) {
+        ssize_t bytesWritten = write(dest_fd, buffer, bytesRead);
+        if (bytesWritten == -1) {
+            perror("Error writing to destination file");
+            close(src_fd);
+            close(dest_fd);
+            return;
+        }
+    }
+
+    // Check if there was an error during reading
+    if (bytesRead == -1) {
+        perror("Error reading source file");
+    }
+
+    // Close the file descriptors
+    close(src_fd);
+    close(dest_fd);
 }
 
 /*for the mv command*/
 void moveFile(char *sourcePath, char *destinationPath) {
     // copy file over
+
     // delete file from source path
     remove(sourcePath);
 }
@@ -97,9 +135,9 @@ void displayFile(char *filename) {
 			
     /* write 80 "-" characters to stdout */
 			
-	for(int i = 0; i < 80; i ++){
-		write(STDOUT_FILENO, "-", strlen("-"));
-	}
+	//for(int i = 0; i < 80; i ++){
+	//	write(STDOUT_FILENO, "-", strlen("-"));
+	//}
 	/* close the read file and free/null assign your line buffer */
 	write(STDOUT_FILENO, "\n", 1);
 }
